9c86b06640633f008096b322f38084718c6bacf3
Dakotah Ulrath <dulrath@live.com>
2022-06-12 16:50:14 -0400

    intern object create/test pass

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -3,8 +3,8 @@ const Employee = require('../lib/Employee')
 test('creates and employee object', () => {
     const employee = new Employee('Kollen', 41, 'kollen@gmail.com')
 
-    expect(employee.name).toEqual(String)
-    expect(employee.id).toEqual(Number)
-    expect(employee.email).toEqual(String)
+    expect(employee.name).toEqual(expect.any(String))
+    expect(employee.id).toEqual(expect.any(Number))
+    expect(employee.email).toEqual(expect.any(String))
 
 })
